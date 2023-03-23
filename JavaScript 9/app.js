b12212ff9141d2a1dd38a46ceede03ba260fc8a5
IzzatbekBarotov <123806032+IzzatbekBarotov@users.noreply.github.com>
2023-03-23 14:32:50 +0500

    Add files via upload

---
+++ JavaScript 9/app.js
@@ -0,0 +1,35 @@
+const btnEl = document.getElementById("btn");
+
+function createNoteEl(id, content) {
+  const element = document.createElement("textarea");
+  element.classList.add("note");
+  element.placeholder = "Empty Note";
+  element.value = content;
+
+  element.addEventListener("dblclick", () => {
+    const warning = confirm("Do you want to delete this note?");
+    if (warning) {
+      deleteNote(id, element);
+    }
+  });
+
+  element.addEventListener("input", () => {
+    updateNote(id, element.value);
+  });
+  return element;
+}
+
+function deleteNote() {}
+
+function updateNote() {}
+
+function addNote() {
+  const noteObj = {
+    id: Math.floor(Math.random() * 100000),
+    content: "",
+  };
+  const noteEl = createNoteEl(noteObj.id, noteObj.content);
+  appEl.inserBefore(noteEl, btnEl);
+}
+
+btnEl.addEventListener("click", addNote);
